9bd0d52ea8f6d147957c66618452e77f81d22302
Suman Paul <sumank.paul@gmail.com>
2016-05-03 15:21:51 +0530

    change port

--- server.js
+++ server.js
@@ -5,36 +5,38 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
-    host: '0.0.0.0',
-    port: +process.env.PORT
+  host: '0.0.0.0',
+  port: +process.env.PORT
 });
 
 // Add the route
 server.route({
-    method: 'GET',
-    path:'/',
-    handler: function (request, reply) {
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
 
-        return reply('hello world');
-    }
+    return reply('hello world');
+  }
 });
-
 
 server.route({
-    method: 'GET',
-    path:'/webhook',
-    handler: function (request, reply) {
+  method: 'GET',
+  path: '/webhook',
+  handler: function (request, reply) {
 
-        return reply('1621569172');
+    if (request.query['hub.verify_token'] === 'EAAMdMrzztUMBAIHGRHpttv7BadmCY96ZAcHnXdDiwRIKKDZCpnWqpShneEM0sP6avJA7AhlZBGGInxt3ZCMIhduaFBRj6VmcQSiKF5e4XFJeaKiFA95GMH04t6TaEBlax1cyBDP621r5ITjmJuZCKTyyD2sTtpkBJxcRpy2JwhgZDZD') {
+      return reply(request.query['hub.challenge']);
     }
+    return reply('Error, wrong validation token');
+  }
 });
-
 
 // Start the server
-server.start((err) => {
+server.start((err) = > {
 
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
-});
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
+})
+;
