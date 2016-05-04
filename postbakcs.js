c06dab2b31e0f2ef7f69489867fe2b0f734237e9
Suman Paul <sumank.paul@gmail.com>
2016-05-05 01:14:17 +0530

    new deploy

--- postbakcs.js
+++ postbakcs.js
@@ -1,5 +1,7 @@
 (function () {
 
+  var request = require('request');
+
   var token = "EAAMdMrzztUMBAIHGRHpttv7BadmCY96ZAcHnXdDiwRIKKDZCpnWqpShneEM0sP6avJA7AhlZBGGInxt3ZCMIhduaFBRj6VmcQSiKF5e4XFJeaKiFA95GMH04t6TaEBlax1cyBDP621r5ITjmJuZCKTyyD2sTtpkBJxcRpy2JwhgZDZD";
 
   var dispatchRequest = function (message, sender) {
@@ -19,7 +21,7 @@
       }
     });
   };
-  
+
   var postbacks = {
     services: function (sender) {
       var messageData = {
