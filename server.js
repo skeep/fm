affb8c98f8dea78dc5a0aa668a73183b2737f1ca
Suman Paul <sumank.paul@gmail.com>
2016-05-04 22:39:50 +0530

    new deploy

--- server.js
+++ server.js
@@ -30,28 +30,6 @@ var bill = {
               "payload": "USER_DEFINED_PAYLOAD_FOR_ITEM100"
             }
           ]
-        },
-        {
-          "title": "Classic Grey T-Shirt",
-          "image_url": "http://petersapparel.parseapp.com/img/item101-thumb.png",
-          "subtitle": "Soft gray cotton t-shirt is back in style",
-          "buttons": [
-            {
-              "type": "web_url",
-              "url": "https://petersapparel.parseapp.com/view_item?item_id=101",
-              "title": "View Item"
-            },
-            {
-              "type": "web_url",
-              "url": "https://petersapparel.parseapp.com/buy_item?item_id=101",
-              "title": "Buy Item"
-            },
-            {
-              "type": "postback",
-              "title": "Bookmark Item",
-              "payload": "USER_DEFINED_PAYLOAD_FOR_ITEM101"
-            }
-          ]
         }
       ]
     }
