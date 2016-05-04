14babe8e44fd9c2e2b0b471f74a7768fa9165499
Suman Paul <sumank.paul@gmail.com>
2016-05-05 01:13:16 +0530

    new deploy

--- postbakcs.js
+++ postbakcs.js
@@ -1,4 +1,25 @@
 (function () {
+
+  var token = "EAAMdMrzztUMBAIHGRHpttv7BadmCY96ZAcHnXdDiwRIKKDZCpnWqpShneEM0sP6avJA7AhlZBGGInxt3ZCMIhduaFBRj6VmcQSiKF5e4XFJeaKiFA95GMH04t6TaEBlax1cyBDP621r5ITjmJuZCKTyyD2sTtpkBJxcRpy2JwhgZDZD";
+
+  var dispatchRequest = function (message, sender) {
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: token},
+      method: 'POST',
+      json: {
+        recipient: {id: sender},
+        message: message,
+      }
+    }, function (error, response, body) {
+      if (error) {
+        console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+      }
+    });
+  };
+  
   var postbacks = {
     services: function (sender) {
       var messageData = {
