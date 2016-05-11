d9f107346150736da743c000751b492bee5dac08
Suman Paul <sumank.paul@gmail.com>
2016-05-11 16:27:46 +0530

    new deploy

--- bank/validators.js
+++ bank/validators.js
@@ -10,29 +10,29 @@ var core = require('chanakya'),
 
 core.validator('isStatement', function (message) {
 
-  // var deferred = Q.defer();
-  //
-  // var options = {
-  //   uri: `https://api.wit.ai/message?q=${message}`,
-  //   headers: {
-  //     'Authorization': 'Bearer LUPCP5OOAFQXURYOH7I7YVU4FGBGVAG3',
-  //     'Accept': 'application/vnd.wit.20141022+json'
-  //   },
-  //   json: true
-  // };
-  //
-  // rp(options)
-  //   .then(function (res) {
-  //     deferred.resolve(_.keys(res.outcomes[0].entities)[0]);
-  //   })
-  //   .catch(function (err) {
-  //     console.error(err);
-  //   });
-  //
-  // return deferred.promise;
+  var deferred = Q.defer();
 
-  return Q.fcall(function () {
-    return 'balance';
-  });
+  var options = {
+    uri: `https://api.wit.ai/message?q=${message}`,
+    headers: {
+      'Authorization': 'Bearer LUPCP5OOAFQXURYOH7I7YVU4FGBGVAG3',
+      'Accept': 'application/vnd.wit.20141022+json'
+    },
+    json: true
+  };
+
+  rp(options)
+    .then(function (res) {
+      deferred.resolve(_.keys(res.outcomes[0].entities)[0]);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+
+  return deferred.promise;
+
+  // return Q.fcall(function () {
+  //   return 'balance';
+  // });
 
 });
