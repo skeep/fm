50706fdcdced03246eaa2589e376376ddd1212e2
Gaurav Shukla <gaurav_shukla@mckinsey.com>
2016-05-12 12:11:15 +0530

    new deploy

--- bank/responses.js
+++ bank/responses.js
@@ -40,3 +40,30 @@ core.response('balance', function (to) {
     });
   return deferred.promise;
 }, 'statement');
+
+core.response('creditcard', function (to) {
+
+  console.log(to);
+
+  var deferred = Q.defer();
+
+  var options = {
+    uri: `https://polar-atoll-33216.herokuapp.com/users/1`,
+    headers: {
+      'Authorization': 'Bearer LUPCP5OOAFQXURYOH7I7YVU4FGBGVAG3',
+      'Accept': 'application/vnd.wit.20141022+json'
+    },
+    json: true // Automatically parses the JSON string in the response
+  };
+
+  rp(options)
+    .then(function (res) {
+      deferred.resolve({
+        text: `Your credit balance is = ${res.currency}${res.balance}`
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+  return deferred.promise;
+}, 'statement');
